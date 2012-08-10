b5be131a5241c6c2c5bbe0d474fab637f2e3b277
Jan Lindblom <lindblom.jan@gmail.com>
2012-08-10 12:41:18 +0200

    Adding the MC API library as a submodule and initiating the spark.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 codeigniter-mailchimp-spark
 ===========================
+
+This is a spark wrapper around the CodeIgniter-Library-for-MailChimp-API-v1.3 by @waynhall .
+
+For usage, see:
+
+ * https://github.com/waynhall/CodeIgniter-Library-for-MailChimp-API-v1.3
---
+++ config/mailchimp.php
@@ -0,0 +1,12 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * @author	Jan Lindblom <jan@powcorp.se>
+ * @copyright	Copyright (c) 2012, POW! Corp.
+ * @license	MIT
+ * @version	1.3.0
+ */
+
+$config['apikey'] = '';
+$config['secure'] = false;
+?>
